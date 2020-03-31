419859639b53d7f215c49db5c9eec7bd603373ce
João Câmara <franchini211@gmail.com>
2020-03-31 17:52:13 -0300

    bugs_fixes

--- mobile/src/pages/home/index.js
+++ mobile/src/pages/home/index.js
@@ -1,10 +1,11 @@
-import React from 'react'
-import style from './styles'
+import React from "react";
+import { View, Text } from "react-native";
+import style from "./styles";
 
-export default function Home(){
-    return(
-        <View style={style.body}>
-            <Text>Home</Text>
-        </View>
-    )
+export default function Home() {
+  return (
+    <View style={style.body}>
+      <Text>Home</Text>
+    </View>
+  );
 }
--- mobile/src/pages/home/styles.js
+++ mobile/src/pages/home/styles.js
@@ -1,12 +1,11 @@
-import { StyleSheet } from 'react-native'
-import Constants from 'expo-constants'
+import { StyleSheet } from "react-native";
+import Constants from "expo-constants";
 
 export default StyleSheet.create({
-    body: {
-        backgroundColor: "#000",
-        flex: 1,
-        paddingHorizontal: 24,
-        paddingTop: Constants.statusBarHeight + 20,
-    },
-
-})
+  body: {
+    backgroundColor: "#fff",
+    flex: 1,
+    paddingHorizontal: 24,
+    paddingTop: Constants.statusBarHeight + 20
+  }
+});
