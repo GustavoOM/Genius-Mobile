b3b2d87898ec4ca9da915c841578ac62535cdb6f
GustavoOM <gumartins2001@gmail.com>
2020-03-31 17:33:13 -0300

    Teste Mobile

---
+++ mobile/src/pages/home/index.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import style from './styles'
+
+export default function Home(){
+    return(
+        <View style={style.body}>
+            <Text>Home</Text>
+        </View>
+    )
+}
---
+++ mobile/src/pages/home/styles.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from 'react-native'
+import Constants from 'expo-constants'
+
+export default StyleSheet.create({
+    body: {
+        backgroundColor: "#000",
+        flex: 1,
+        paddingHorizontal: 24,
+        paddingTop: Constants.statusBarHeight + 20,
+    },
+
+})
