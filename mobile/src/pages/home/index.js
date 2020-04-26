aba10955eb42c2029689c04cc457e5e5a237061f
João Câmara <franchini211@gmail.com>
2020-04-26 18:13:02 -0300

    update

--- mobile/src/pages/home/index.js
+++ mobile/src/pages/home/index.js
@@ -1,11 +1,163 @@
-import React from "react";
-import { View, Text } from "react-native";
-import style from "./styles";
+import React, { useState } from "react";
+import { AntDesign, Ionicons } from "@expo/vector-icons";
+
+import { Audio } from "expo-av";
+
+import {
+  Container,
+  ButtonYellow,
+  ButtonGreen,
+  ButtonBlue,
+  ButtonRed,
+  ColumnButton,
+  ContainerButtons,
+  ContainerDescAndInfo,
+  ContainerInfo,
+  RowButton,
+  PlayButton,
+  TextDesc,
+  TextPoints,
+  TextTitle,
+} from "./styles";
 
 export default function Home() {
+  const [popRedButton, setPopRedButton] = useState(false);
+  const [popGreenButton, setPopGreenButton] = useState(false);
+  const [popBlueButton, setPopBlueButton] = useState(false);
+  const [popYellowButton, setPopYellowButton] = useState(false);
+
+  const [playButtonClick, setPlayButtonClick] = useState(false);
+
+  // const soundObject = new Audio.Sound();
+
+  const [sequence, setSequence] = useState([]);
+  const [points, setPoints] = useState(0);
+  const [userSequence, setUserSequence] = useState([]);
+  const [block, setBlock] = useState(false);
+  const [score, setScore] = useState(0);
+
+  function startGame() {
+    setup();
+    add2Sequence();
+  }
+
+  function setup() {
+    setSequence([]);
+    setUserSequence([]);
+    setPoints(0);
+
+    setPlayButtonClick(!playButtonClick);
+  }
+
+  function endGame() {
+    updateScore();
+    setup();
+  }
+
+  function updateScore() {
+    if (points > score) score = points;
+  }
+
+  // try {
+  //   await soundObject.loadAsync(require('./assets/sounds/hello.mp3'));
+  //   await soundObject.playAsync();
+  //   // Your sound is playing!
+  // } catch (error) {
+  //   // An error occurred!
+  // }
+
+  function add2Sequence() {
+    var random = Math.round(Math.random() * (4 - 1) + 1);
+    setSequence([...sequence, random]);
+    console.log(sequence);
+    showSequence();
+  }
+
+  function showSequence(sequence) {
+    for (var i in sequence) {
+      switch (i) {
+        case 1: //Green
+          break;
+        case 2: //Red
+          break;
+        case 3: //Yellow
+          break;
+        case 4: //Blue
+          break;
+        default:
+          break;
+      }
+    }
+    setBlock(false);
+  }
+
+  function addUserSequence(color) {
+    setUserSequence([...userSequence, color]);
+    verifyUserSequence();
+  }
+
+  function verifyUserSequence() {
+    for (var i = 0; i < userSequence.length; i++) {
+      if (userSequence[i] !== sequence[i]) {
+        endGame();
+        return;
+      }
+    }
+
+    setPoints(points + 1);
+    setUserSequence([]);
+    if (sequence.length === userSequence.lenght) {
+      setBlock(true);
+      add2Sequence();
+    }
+  }
+
   return (
-    <View style={style.body}>
-      <Text>Home</Text>
-    </View>
+    <Container>
+      <TextTitle>ESPERTUS</TextTitle>
+      <ContainerButtons>
+        <ColumnButton>
+          <RowButton>
+            <ButtonGreen
+              popButton={popGreenButton}
+              onPress={() => (block === false ? addUserSequence(1) : null)}
+            />
+            <ButtonRed
+              popButton={popRedButton}
+              onPress={() => (block === false ? addUserSequence(2) : null)}
+            />
+          </RowButton>
+          <RowButton>
+            <ButtonYellow
+              popButton={popYellowButton}
+              onPress={() => (block === false ? addUserSequence(3) : null)}
+            />
+            <ButtonBlue
+              popButton={popBlueButton}
+              onPress={() => (block === false ? addUserSequence(4) : null)}
+            />
+          </RowButton>
+        </ColumnButton>
+        <PlayButton
+          onPress={() => (playButtonClick === false ? startGame() : endGame())}
+        >
+          {playButtonClick === false ? (
+            <AntDesign name="play" size={100} color="#999" />
+          ) : (
+            <Ionicons name="md-refresh-circle" size={123} color="#999" />
+          )}
+        </PlayButton>
+      </ContainerButtons>
+      <ContainerInfo>
+        <ContainerDescAndInfo>
+          <TextDesc>PONTOS</TextDesc>
+          <TextPoints>{points < 10 ? `0${points}` : points}</TextPoints>
+        </ContainerDescAndInfo>
+        <ContainerDescAndInfo>
+          <TextDesc>RECORDE</TextDesc>
+          <TextPoints>{score < 10 ? `0${score}` : score}</TextPoints>
+        </ContainerDescAndInfo>
+      </ContainerInfo>
+    </Container>
   );
 }
--- mobile/src/pages/home/styles.js
+++ mobile/src/pages/home/styles.js
@@ -1,11 +1,132 @@
-import { StyleSheet } from "react-native";
-import Constants from "expo-constants";
+import styled from "styled-components/native";
+import { lighten } from "polished";
 
-export default StyleSheet.create({
-  body: {
-    backgroundColor: "#fff",
-    flex: 1,
-    paddingHorizontal: 24,
-    paddingTop: Constants.statusBarHeight + 20
-  }
-});
+import { RectButton } from "react-native-gesture-handler";
+
+export const Container = styled.KeyboardAvoidingView`
+  background-color: #888;
+  flex: 1;
+
+  justify-content: center;
+`;
+
+export const PlayButton = styled(RectButton)`
+  position: absolute;
+  width: 120px;
+  height: 120px;
+  background: #62625e;
+  border-radius: 60px;
+  align-items: center;
+  justify-content: center;
+`;
+
+export const ButtonYellow = styled.TouchableOpacity`
+  width: 150px;
+  height: 150px;
+  margin: 5px;
+  background: ${(props) =>
+    !props.popButton ? "#ffe44c" : `${lighten(0.2, "#ffe44c")}`};
+  border-bottom-left-radius: 150px;
+`;
+
+export const ButtonRed = styled.TouchableOpacity`
+  width: 150px;
+  height: 150px;
+  margin: 5px;
+  background: ${(props) =>
+    !props.popButton ? "#fe524b" : `${lighten(0.2, "#fe524b")}`};
+  border-top-right-radius: 150px;
+`;
+
+export const ButtonGreen = styled.TouchableOpacity`
+  width: 150px;
+  height: 150px;
+  margin: 5px;
+  background: ${(props) =>
+    !props.popButton ? "#5df99e" : `${lighten(0.2, "#5df99e")}`};
+  border-top-left-radius: 150px;
+`;
+
+export const ButtonBlue = styled.TouchableOpacity`
+  width: 150px;
+  height: 150px;
+  margin: 5px;
+  background: ${(props) =>
+    !props.popButton ? "#2bd9fe" : `${lighten(0.2, "#2bd9fe")}`};
+  border-bottom-right-radius: 150px;
+`;
+
+export const RowButton = styled.View`
+  flex-direction: row;
+`;
+
+export const ColumnButton = styled.View`
+  flex-direction: column;
+  background-color: #62625e;
+  border-radius: 20px;
+  padding: 5px;
+`;
+
+export const ContainerButtons = styled.View`
+  align-items: center;
+  justify-content: center;
+`;
+
+export const TextTitle = styled.Text`
+  padding: 10px 0 10px 0;
+  font-weight: bold;
+  font-size: 65px;
+
+  align-items: center;
+  text-align: center;
+
+  color: rgba(250, 250, 250, 1);
+
+  text-shadow: 1px 1px 1px rgba(0, 0, 0, 0.09);
+  color: rgba(250, 250, 250, 0.71);
+
+  text-shadow: 2.19311px 4.38622px 2.19311px rgba(0, 0, 0, 0.08);
+`;
+
+export const TextPoints = styled.Text`
+  padding: 10px 0 10px 0;
+
+  font-weight: bold;
+  font-size: 50px;
+
+  align-items: center;
+  text-align: center;
+
+  color: rgba(250, 250, 250, 0.71);
+
+  text-shadow: 2.19311px 4.38622px 2.19311px rgba(0, 0, 0, 0.08);
+`;
+
+export const TextDesc = styled.Text`
+  padding: 10px 0 10px 0;
+
+  font-weight: bold;
+  font-size: 35px;
+
+  align-items: center;
+  text-align: center;
+
+  color: rgba(250, 250, 250, 0.71);
+
+  text-shadow: 2.19311px 4.38622px 2.19311px rgba(0, 0, 0, 0.08);
+`;
+
+export const ContainerInfo = styled.View`
+  align-items: center;
+  justify-content: center;
+  flex-direction: row;
+
+  padding: 0 20px 0 20px;
+
+  justify-content: space-between;
+`;
+
+export const ContainerDescAndInfo = styled.View`
+  align-items: center;
+  justify-content: center;
+`;
